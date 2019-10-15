e2fd2d3db8003bf3af56a7ddd34d591970bb9742
Joe Reeve <joseph.reeve@googlemail.com>
2019-10-15 10:21:58 +0100

    :tada: INIT

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import { WriteStream, createWriteStream } from 'fs';
+
+export enum StructuredFormat {
+    JSON = 'json',
+    CSV = 'csv',
+}
+
+export class StructuredStreamWriter {
+    private entries = 0;
+    private columns: string[];
+    private format: StructuredFormat;
+    private filePath: string;
+    private fileStream: WriteStream;
+
+    constructor(format: StructuredFormat, filePath: string, columns?: string[]) {
+        this.format = format;
+        this.filePath = filePath;
+        this.fileStream = createWriteStream(filePath);
+
+        if (format === StructuredFormat.CSV && !columns) {
+            throw new Error(`Writing a CSV file, but don't have columns`);
+        }
+        this.columns = columns;
+
+        this.writeHeader();
+    }
+
+    public done() {
+        this.writeFooter();
+        this.fileStream.close();
+    }
+
+    public writeItem(item: { [key: string]: any }) {
+        return new Promise((resolve, reject) => {
+            if (this.format === StructuredFormat.JSON) {
+                // TODO: add option for pretty/compact JSON
+                const shouldAddPrefixComma = this.entries !== 0;
+
+                const itemString = `${shouldAddPrefixComma ? ',\n' : ''}${JSON.stringify(
+                    item,
+                    null,
+                    '\t',
+                )}`;
+                this.fileStream.write(itemString, (err) => err ? reject(err) : resolve());
+            } else if (this.format === StructuredFormat.CSV) {
+                const itemString = this.columns.map((key) => item[key]).join(',') + '\n';
+                this.fileStream.write(itemString, (err) => err ? reject(err) : resolve());
+            }
+
+            this.entries += 1;
+        });
+    }
+
+    private writeHeader() {
+        let headerString;
+        if (this.format === StructuredFormat.JSON) {
+            headerString = `[\n`;
+        } else if (this.format === StructuredFormat.CSV) {
+            if (this.columns) {
+                headerString = this.columns.join(',') + '\n';
+            }
+        }
+
+        this.fileStream.write(headerString);
+    }
+
+    private writeFooter() {
+        let footerString = '';
+        if (this.format === StructuredFormat.JSON) {
+            footerString = '\n]';
+        }
+
+        this.fileStream.write(footerString);
+    }
+}
---
+++ test/index.test.ts
@@ -0,0 +1,69 @@
+import { StructuredStreamWriter, StructuredFormat } from '../';
+import test from 'blue-tape';
+import { join } from 'path';
+import { tmpdir } from 'os';
+import { readFileSync } from 'fs';
+
+test('JSON Stream Writer', async (t) => {
+    const outPath = join(tmpdir(), `test-${Date.now()}.json`);
+    const ssw = new StructuredStreamWriter(StructuredFormat.JSON, outPath);
+
+    await ssw.writeItem({
+        item: 1,
+        text: 'one',
+    });
+    await ssw.writeItem({
+        item: 2,
+        text: 'two',
+    });
+    await ssw.writeItem({
+        item: 3,
+        text: 'three',
+    });
+    await ssw.writeItem({
+        item: 4,
+        text: 'four',
+    });
+
+    ssw.done();
+    t.ok(`Successfully written 4 items to [${outPath}]`);
+
+    const data = require(outPath);
+    t.equal(data.length, 4, `Correct number of items written`);
+    t.equal(data[0].item, 1, `First item is correct`);
+    t.equal(data[3].item, 4, `Last item is correct`);
+});
+
+test('CSV Stream Writer', async (t) => {
+    const outPath = join(tmpdir(), `test-${Date.now()}.csv`);
+    const ssw = new StructuredStreamWriter(StructuredFormat.CSV, outPath, ['item', 'text']);
+
+    await ssw.writeItem({
+        item: 1,
+        text: 'one',
+    });
+    await ssw.writeItem({
+        item: 2,
+        text: 'two',
+    });
+    await ssw.writeItem({
+        item: 3,
+        text: 'three',
+    });
+    await ssw.writeItem({
+        item: 4,
+        text: 'four',
+    });
+
+    ssw.done();
+    t.ok(`Successfully written 4 items to [${outPath}]`);
+
+    const data = readFileSync(outPath, 'UTF8').trim().split('\n');
+    t.equal(data.length, 5, `Correct number of items written (4 + header)`);
+    t.equal(data[0], `item,text`, `Header written correctly`);
+    t.equal(data[4], `4,four`, `Last row written correctly`);
+
+    await t.throws(() => {
+        new StructuredStreamWriter(StructuredFormat.CSV, outPath);
+    }, `Fails to create CSV SSW with no headers`);
+});
